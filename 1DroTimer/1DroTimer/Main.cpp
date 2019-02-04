cd1e220b626f5854e765bfc0643f4d68a8085183
Vii <a@b>
2019-02-04 14:16:03 +0900

    taskbar progress: change color

--- 1DroTimer/1DroTimer/Main.cpp
+++ 1DroTimer/1DroTimer/Main.cpp
@@ -15,7 +15,37 @@
 #define NK_GDI_IMPLEMENTATION
 #include "nuklear.h"
 #include "nuklear_gdi.h"
+
 LPCWSTR appName = TEXT(L"1DroTimer");
+
+static class TBP {
+	ITaskbarList3 *ppv;
+public:
+	void Init() {
+		CLSID clsid;
+		IID iid;
+		if (!SUCCEEDED(CLSIDFromString(OLESTR("{56FDF344-FD6D-11d0-958A-006097C9A090}"), &clsid))) return;
+		if (!SUCCEEDED(IIDFromString(OLESTR("{EA1AFB91-9E28-4B86-90E9-9E9F8A5EEFAF}"), &iid))) return;
+		if (!SUCCEEDED(CoCreateInstance(clsid, NULL, CLSCTX_ALL, iid, (void **)&ppv))) return;
+	}
+	void Set(HWND hWnd, int completed, int total) {
+		if (!ppv) return;
+		ppv->SetProgressValue(hWnd, completed, total);
+		double r = (double)completed / total;
+		if (r < 0.5) ppv->SetProgressState(hWnd, TBPF_PAUSED);
+		if (r < 0.2) ppv->SetProgressState(hWnd, TBPF_ERROR);
+	}
+	void Notify(HWND hWnd) {
+		if (!ppv) return;
+		ppv->SetProgressState(hWnd, TBPF_ERROR);
+		ppv->SetProgressValue(hWnd, 1, 1);
+	}
+	void Reset(HWND hWnd) {
+		if (!ppv) return;
+		ppv->SetProgressValue(hWnd, 0, 0);
+		ppv->SetProgressState(hWnd, TBPF_NORMAL);
+	}
+} tbp;
 
 static struct Timer {
 	enum {
@@ -42,31 +72,17 @@ static struct Timer {
 				state = STOP;
 				KillTimer(hWnd, id);
 				//Beep(440, 1000);
+				tbp.Notify(hWnd);
 			//	if (notify) {
-					MessageBoxW(hWnd, TEXT(L"Time up!"), appName, MB_OK);
+					MessageBoxW(hWnd, TEXT(L"Time up!"), appName, MB_OK );
 			//		notify = false;
 			//	}
+					tbp.Reset(hWnd);
 			}
 		}
 	}
 	Timer(int id_) : id(id_), notify(0) {}
-} timer(1);
-
-static class TBP {
-	CLSID clsid;
-	IID iid;
-	ITaskbarList3 *ppv;
-public:
-	void Init() {
-		CLSIDFromString(OLESTR("{56FDF344-FD6D-11d0-958A-006097C9A090}"), &clsid);
-		IIDFromString(OLESTR("{EA1AFB91-9E28-4B86-90E9-9E9F8A5EEFAF}"), &iid);
-		CoCreateInstance(clsid, NULL, CLSCTX_ALL, iid, (void **)&ppv);
-	}
-	void Set(HWND hWnd, int completed, int total) {
-		ppv->SetProgressValue(hWnd, completed, total);
-	}
-} tbp;
-
+} timer(196884);
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 	switch (msg) {
@@ -91,7 +107,7 @@ int WinMain(HINSTANCE hInst, HINSTANCE, LPSTR psCmdLine, int nCmdShow) {
 	wc.hInstance = GetModuleHandleW(0);
 	//wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.lpszClassName = L"WindowClass";
+	wc.lpszClassName = L"1DRO_TIMER";
 	RegisterClassW(&wc);
 
 	HWND hWnd = CreateWindowExW(WS_EX_APPWINDOW, wc.lpszClassName, appName,
@@ -102,87 +118,74 @@ int WinMain(HINSTANCE hInst, HINSTANCE, LPSTR psCmdLine, int nCmdShow) {
 	HDC hDC = GetDC(hWnd);
 
 	GdiFont* font = nk_gdifont_create("Arial", 14);
-	GdiFont* f2 = nk_gdifont_create("Arial", 24);
+	//GdiFont* f2 = nk_gdifont_create("Arial", 24);
 	nk_context *ctx = nk_gdi_init(font, hDC, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-	int needs_refresh = 1;
-	for (bool running = true; running; ) {
+	tbp.Init();
+
+	for (bool running = true, refresh = true; running; ) {
 		/* Input */
 		MSG msg;
 		nk_input_begin(ctx);
-		if (needs_refresh == 0) {
+		if (!refresh) {
 			if (GetMessageW(&msg, NULL, 0, 0) <= 0) running = false;
 			else {
 				TranslateMessage(&msg);
 				DispatchMessageW(&msg);
 			}
-			needs_refresh = 1;
+			refresh = true;
 		}
-		else needs_refresh = 0;
+		else refresh = false;
 		while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
 			if (msg.message == WM_QUIT) running = false;
 			TranslateMessage(&msg);
 			DispatchMessageW(&msg);
-			needs_refresh = 1;
+			refresh = true;
 		}
-
 		nk_input_end(ctx);
+
 		RECT rc;
 		GetClientRect(hWnd, &rc);
 		int width = rc.right - rc.left;
 		int height = rc.bottom - rc.top;
 
 		/* GUI */
-		if (nk_begin(ctx, "", nk_rect(0, 0, width, height), NK_WINDOW_NO_SCROLLBAR))
-		{
-			nk_window_set_size(ctx, "", nk_vec2(rc.right - rc.left, rc.bottom - rc.top));
+		if (nk_begin(ctx, "", nk_rect(0, 0, width, height), NK_WINDOW_NO_SCROLLBAR)) {
+			nk_window_set_size(ctx, "", nk_vec2(width, height));
 
-			//
 			static const float ratio[] = { 60, 150 };
 			{
-				//set_style(ctx, THEME_WHITE);
 				nk_layout_row(ctx, NK_STATIC, 30, 2, ratio);
 				nk_labelf(ctx, NK_TEXT_RIGHT, "Hours: %02d", timer.h);
 				nk_progress(ctx, (size_t *)&timer.h, 100, NK_MODIFIABLE);
-				//set_style(ctx, THEME_BLUE);
 				nk_labelf(ctx, NK_TEXT_RIGHT, "Minutes: %02d", timer.m);
 				nk_progress(ctx, (size_t *)&timer.m, 60, NK_MODIFIABLE);
 				nk_labelf(ctx, NK_TEXT_RIGHT, "Seconds: %02d", timer.s);
 				nk_progress(ctx, (size_t *)&timer.s, 60, NK_MODIFIABLE);
 			}
 
-			float r2[] = {150, 60};
-			switch (timer.state) {
-			case Timer::STOP:
-				nk_layout_row_static(ctx, 60, 150+60+4, 1);
-				if (nk_button_label(ctx, "Start")) {
-					timer.Start(hWnd);
-					tbp.Init();
-				}
-				break;
-			case Timer::COUNTING:
-				nk_layout_row(ctx, NK_STATIC, 60, 2, r2);
-				if (nk_button_label(ctx, "Pause")) timer.state = Timer::PAUSE;
-				if (nk_button_label(ctx, "Stop")) timer.state = Timer::STOP;
-				//if (nk_button_symbol(ctx, NK_SYMBOL_RECT_SOLID)) timer.state = Timer::TS_STOP;
-				break;
-			case Timer::PAUSE:
-				nk_layout_row(ctx, NK_STATIC, 60, 2, r2);
-				if (nk_button_label(ctx, "Continue")) timer.state = Timer::COUNTING;
-				if (nk_button_label(ctx, "Stop")) timer.state = Timer::STOP;
-				break;
-			}
-
-			if (timer.state != Timer::STOP) {
+			static const float r2[] = { 150, 60 };
+			if (timer.state == Timer::STOP) {
+				nk_layout_row_static(ctx, 60 + 30 + 4, r2[0] + r2[1] + 4, 1);
+				if (nk_button_label(ctx, "Start")) timer.Start(hWnd);
+			} else {
 				nk_style_item prev = ctx->style.progress.cursor_normal;
-				ctx->style.progress.cursor_normal = nk_style_item_color(nk_rgba(50, 230, 150, 255));	// BAR
-
-				nk_layout_row(ctx, NK_STATIC, 30, 2, ratio);
-				nk_labelf(ctx, NK_TEXT_RIGHT, "%02d:%02d'%02d\"", timer.restTime / 3600, (timer.restTime / 60) % 60, timer.restTime % 60);
-				nk_progress(ctx, (size_t *)&timer.restTime, timer.initTime, 0);
-				tbp.Set(hWnd, timer.restTime, timer.initTime);
-
+				ctx->style.progress.cursor_normal = nk_style_item_color(nk_rgba(50, 230, 150, 255));	// Green BAR
+					nk_layout_row(ctx, NK_STATIC, 30, 2, ratio);
+					nk_labelf(ctx, NK_TEXT_RIGHT, "%02d:%02d'%02d\"", timer.restTime / 3600, (timer.restTime / 60) % 60, timer.restTime % 60);
+					nk_progress(ctx, (size_t *)&timer.restTime, timer.initTime, 0);
+					tbp.Set(hWnd, timer.restTime, timer.initTime);
 				ctx->style.progress.cursor_normal = prev;
+
+				nk_layout_row(ctx, NK_STATIC, 60, 2, r2);
+				if(timer.state == Timer::COUNTING)
+					if (nk_button_label(ctx, "Pause")) timer.state = Timer::PAUSE;
+				else if (timer.state == Timer::PAUSE)
+					if (nk_button_label(ctx, "Continue")) timer.state = Timer::COUNTING;
+				if (nk_button_label(ctx, "Stop")) {
+					timer.state = Timer::STOP;
+					tbp.Reset(hWnd);
+				}
 			}
 		}
 		nk_end(ctx);
